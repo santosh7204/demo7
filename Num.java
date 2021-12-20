e63802a7813e402b1e0d246a22a982e2cee862bf
santosh <santuyanagunti23.xworkz@gmail.com>
2021-12-20 22:18:46 +0530

    num pro

---
+++ Num.java
@@ -0,0 +1,16 @@
+class Num{
+  public static void main(String[] args){
+	  byte[] age={21,23,34,53,71,32,67,5,80,10,-100};
+	  System.out.println(age[0]);
+      System.out.println(age[1]);
+      System.out.println(age[2]);
+	  System.out.println(age[3]); 
+	  System.out.println(age[4]);
+      System.out.println(age[5]);
+	  System.out.println(age[6]);
+	  System.out.println(age[7]);
+	  System.out.println(age[8]);
+	  System.out.println(age[9]);
+      System.out.println(age[10]);
+  }
+}
